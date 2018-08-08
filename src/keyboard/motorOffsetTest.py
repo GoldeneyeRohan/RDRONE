064580f449728e303fa6d9457745045edaea1ab2
GoldeneyeRohan <rohan.sinha@berkeley.edu>
2018-08-08 09:16:55 +0000

    fixed key node

--- src/keyboard/motorOffsetTest.py
+++ src/keyboard/motorOffsetTest.py
@@ -5,7 +5,6 @@ from RDRONE.msg import Throttle
 
 def setThrottle(char, throttle): 
     if (throttle <= 1780) and (char == 'a'):
-        print('a')
         return throttle + 20
     elif (throttle >= 1220) and (char == 'd'):
         return throttle - 20
@@ -13,24 +12,24 @@ def setThrottle(char, throttle):
         return throttle
 
 def key_node():
-    rospy.init_node('key_node')
     pub = rospy.Publisher('throttle_cmd', Throttle)
+    rospy.init_node('key_node')
     throttle = 1200
+    print('RDRONE constant motor offset test node. ')
     print('Use A to increment trottle by 20, use D to decrement throttle by 20 (range is 1200-1800)')
+    print('Use F to exit')
     while not rospy.is_shutdown():
         msg = Throttle()
         msg.FL = throttle
         msg.FR = throttle
         msg.RR = throttle
         msg.RL = throttle
-        print('publishing msg')
         pub.publish(msg)
-        print('published msg')
         char = getch.getch()
         throttle = setThrottle(char,throttle)
         if char == 'f':
-            exit(0
-)
+            exit(0)
+
 if __name__=="__main__":
     try:
         key_node()
