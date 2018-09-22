017edeef5e48d8fbccaf4e0fec33473057fd4c3e
GoldeneyeRohan <rohan.sinha@berkeley.edu>
2018-09-22 03:37:45 +0000

    individual offset actuation

--- src/keyboard/motorOffsetTest.py
+++ src/keyboard/motorOffsetTest.py
@@ -3,30 +3,37 @@ import getch
 import rospy
 from RDRONE.msg import Throttle
 
-def setThrottle(char, throttle): 
-    if (throttle <= 1780) and (char == 'a'):
-        return throttle + 20
-    elif (throttle >= 1220) and (char == 'd'):
-        return throttle - 20
+def setThrottle(char, throttleFront, throttleRear): 
+    if (throttleFront <= 1780) and (char == 'a'):
+        return throttleFront + 20, throttleRear
+    elif (throttleFront >= 1220) and (char == 'd'):
+        return throttleFront - 20, throttleRear
+    elif (throttleRear <= 1780) and (char == 'j'):
+        return throttleFront, throttleRear + 20
+    elif (throttleRear >= 1220) and (char == 'l'):
+        return throttleFront, throttleRear - 20
+
     else: 
-        return throttle
+        return throttleFront, throttleRear
 
 def key_node():
     pub = rospy.Publisher('throttle_cmd', Throttle)
     rospy.init_node('key_node')
-    throttle = 1200
+    throttleFront = 1200
+    throttleRear = 1200
     print('RDRONE constant motor offset test node. ')
     print('Use A to increment trottle by 20, use D to decrement throttle by 20 (range is 1200-1800)')
     print('Use F to exit')
     while not rospy.is_shutdown():
         msg = Throttle()
-        msg.FL = throttle
-        msg.FR = throttle
-        msg.RR = throttle
-        msg.RL = throttle
+        print(throttleFront, throttleRear)
+        msg.FL = throttleFront
+        msg.FR = throttleFront
+        msg.RR = throttleRear
+        msg.RL = throttleRear
         pub.publish(msg)
         char = getch.getch()
-        throttle = setThrottle(char,throttle)
+        throttleFront, throttleRear = setThrottle(char,throttleFront, throttleRear)
         if char == 'f':
             exit(0)
 
